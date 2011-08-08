253a6aa6f7550d6199f9e27ab40abeb58a0b1666
Aaron Quint <aaron@quirkey.com>
2011-08-08 10:35:51 -0700

    Install script just patches the files

--- lib/rails-boot-reporting.rb
+++ lib/rails-boot-reporting.rb
@@ -1,2 +1,34 @@
-require 'bundler-1.0/runtime'
-require 'rails-2.3.11/initializer'
+require 'bundler'
+
+module RailsBootReporting
+  extend self
+
+  def install
+    install_rails
+    install_bundler
+    puts "Done."
+  end
+
+  def install_rails
+    puts "-- Installing Rails patches"
+    install_file('rails-2.3.11/initializer.rb', gem_path('rails', '2.3.11', 'initializer.rb'))
+  end
+
+  def install_bundler
+    puts "-- Installing Bundler patches"
+    bundler_version = Bundler::Version
+    install_file('bundler-1.0/runtime.rb', gem_path('bundler', bundler_version, 'bundler/runtime.rb'))
+  end
+
+
+  def gem_path(gem, version, file)
+    install_path = File.join(ENV['GEM_HOME'], 'gems', "#{gem}-#{version}", 'lib', file)
+  end
+
+  def install_file(local_path, install_path)
+    local_path = File.join(File.dirname(File.expand_path(__FILE__)), local_path)
+    puts "Copying #{local_path} to #{install_path}"
+    FileUtils.cp(local_path, install_path)
+  end
+
+end
